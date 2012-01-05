1fd803d70c06ae06aaced41305633a79df19089a
Shireesh Anjal <shireesh@gluster.com>
2012-01-05 19:12:27 +0530

    Gluster Business entity GlusterVolume

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/GlusterEntity.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2011 Gluster, Inc. <http://www.gluster.com>
+ * This file is part of Gluster Management Console.
+ *
+ * Gluster Management Console is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Gluster Management Console is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package org.ovirt.engine.core.common.businessentities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.ovirt.engine.core.common.utils.StringUtil;
+
+@XmlSeeAlso({GlusterVolume.class, GlusterBrick.class, GlusterVolumeOption.class})
+@XmlRootElement
+public class GlusterEntity implements Filterable {
+
+	protected String name;
+	protected List<GlusterEntity> children = new ArrayList<GlusterEntity>();
+	private GlusterEntity parent;
+
+	public GlusterEntity() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@XmlTransient
+	public GlusterEntity getParent() {
+		return parent;
+	}
+
+	public void setParent(GlusterEntity parent) {
+		this.parent = parent;
+	}
+
+	public List<? extends GlusterEntity> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<GlusterEntity> children) {
+		this.children = children;
+	}
+
+	public GlusterEntity(String name, GlusterEntity parent) {
+		this.name = name;
+		this.parent = parent;
+	}
+
+	public GlusterEntity(String name, GlusterEntity parent, List<GlusterEntity> children) {
+		this.name = name;
+		this.parent = parent;
+		this.children = children;
+	}
+	
+	@Override
+	public boolean filter(String filterString, boolean caseSensitive) {
+		return StringUtil.filterString(getName(), filterString, caseSensitive);
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+}
---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/GlusterVolumeOption.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2011 Gluster, Inc. <http://www.gluster.com>
+ * This file is part of Gluster Management Console.
+ *
+ * Gluster Management Console is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Gluster Management Console is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package org.ovirt.engine.core.common.businessentities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.ovirt.engine.core.common.utils.StringUtil;
+
+/**
+ *
+ */
+@XmlRootElement(name = "GlusterVolumeOption")
+public class GlusterVolumeOption implements Filterable {
+	private String key;
+	private String value;
+
+	public GlusterVolumeOption() {
+	}
+	
+	public GlusterVolumeOption(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return key + "=" + value;
+	}
+
+	@Override
+	public boolean filter(String filterString, boolean caseSensitive) {
+		return StringUtil.filterString(getKey() + getValue(), filterString, caseSensitive);
+	}
+}
