1902a8d7ac42ff220d2cd6065635652275c70183
Shireesh Anjal <shireesh@gluster.com>
2012-01-08 16:06:36 +0530

    Modified to check permissions at cluster level.

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/CreateGlusterVolumeCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/CreateGlusterVolumeCommand.java
@@ -3,10 +3,12 @@
  */
 package org.ovirt.engine.core.bll.gluster;
 
+import java.util.Collections;
 import java.util.Map;
 
 import org.ovirt.engine.core.bll.Backend;
 import org.ovirt.engine.core.bll.CommandBase;
+import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.action.CreateGlusterVolumeParameters;
 import org.ovirt.engine.core.common.vdscommands.CreateGlusterVolumeVDSParameters;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
@@ -20,12 +22,13 @@ public class CreateGlusterVolumeCommand<T extends CreateGlusterVolumeParameters>
 
     CreateGlusterVolumeCommand(T params) {
         super(params);
+        setVdsGroupId(params.getVdsGroupId());
     }
 
     @Override
     protected boolean canDoAction() {
         boolean canDoAction = super.canDoAction();
-        if (canDoAction && getStoragePoolId() == null) {
+        if (canDoAction && getVdsGroup() == null) {
             canDoAction = false;
             addCanDoActionMessage(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_POOL_NOT_EXIST);
             addCanDoActionMessage(VdcBllMessages.VAR__ACTION__CREATE);
@@ -58,8 +61,6 @@ public class CreateGlusterVolumeCommand<T extends CreateGlusterVolumeParameters>
      */
     @Override
     public Map getPermissionCheckSubjects() {
-        // return Collections.singletonMap(MultiLevelAdministrationHandler.SYSTEM_OBJECT_ID, VdcObjectType.System);
-        // TODO: What do we return if we want to allow any user who has "Create Volume" permission at SYSTEM level?
-        return null;
+        return Collections.singletonMap(getVdsGroupId(), VdcObjectType.VdsGroups);
     }
 }
