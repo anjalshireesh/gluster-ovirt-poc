eb67589b6b368e027fa8e6b6671333aad8147a4a
Shireesh Anjal <shireesh@gluster.com>
2012-01-09 23:37:44 +0530

    Made the constructor public, and enhanced method ExecuteIrsBrokerCommand to pass correct parameters to the VDSM command

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/CreateGlusterVolumeVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/CreateGlusterVolumeVDSCommand.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.ovirt.engine.core.common.businessentities.GlusterVolumeEntity;
-import org.ovirt.engine.core.common.utils.StringUtil;
 import org.ovirt.engine.core.common.vdscommands.CreateGlusterVolumeVDSParameters;
 
 /**
@@ -15,7 +14,7 @@ import org.ovirt.engine.core.common.vdscommands.CreateGlusterVolumeVDSParameters
  */
 public class CreateGlusterVolumeVDSCommand<T extends CreateGlusterVolumeVDSParameters> extends IrsBrokerCommand<CreateGlusterVolumeVDSParameters> {
 
-    protected CreateGlusterVolumeVDSCommand(T parameters) {
+    public CreateGlusterVolumeVDSCommand(T parameters) {
         super(parameters);
     }
 
@@ -25,17 +24,22 @@ public class CreateGlusterVolumeVDSCommand<T extends CreateGlusterVolumeVDSParam
 
         Map<String, Object> parameters = new HashMap<String, Object>();
 
-        parameters.put("name", volume.getName());
+        parameters.put("volumeName", volume.getName());
         parameters.put("replicaCount", volume.getReplicaCount());
         parameters.put("stripeCount", volume.getStripeCount());
-        parameters.put("volumeType", volume.getVolumeType().toString());
         parameters.put("transportType", volume.getTransportType().toString());
-        parameters.put("bricks", StringUtil.collectionToString(volume.getBricks(), ","));
-        parameters.put("options", StringUtil.collectionToString(volume.getOptions().getOptions(), ","));
-        parameters.put("accessProtocols", StringUtil.collectionToString(volume.getAccessProtocols(), ","));
-        parameters.put("accessControlList", volume.getAccessControlList());
-        parameters.put("cifsUsers", StringUtil.collectionToString(volume.getCifsUsers(), ","));
+        parameters.put("bricks", volume.getBrickDirectories().toArray());
 
-        getIrsProxy().glusterVolumeCreate(parameters);
+        status = getIrsProxy().glusterVolumeCreate(parameters);
+
+        // IMPORTANT! This handles errors if any
+        ProceedProxyReturnValue();
+
+        // TODO: invoke additional calls to set options, access protocols, access control list and CIFS users (if required)
+//        parameters.put("volumeType", volume.getVolumeType().toString());
+//        parameters.put("options", StringUtil.collectionToString(volume.getOptions().getOptions(), ","));
+//        parameters.put("accessProtocols", StringUtil.collectionToString(volume.getAccessProtocols(), ","));
+//        parameters.put("accessControlList", volume.getAccessControlList());
+//        parameters.put("cifsUsers", StringUtil.collectionToString(volume.getCifsUsers(), ","));
     }
 }
