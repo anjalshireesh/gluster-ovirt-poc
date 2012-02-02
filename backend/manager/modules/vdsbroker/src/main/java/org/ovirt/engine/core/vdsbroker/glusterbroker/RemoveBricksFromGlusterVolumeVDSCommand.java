5d40479d98c4404bcd179b9c6cc66f9e3ada37dd
Dhandpani Gopal <dgopal@redhat.com>
2012-02-02 11:28:04 +0530

    New VDS Remove Brick Command
    Moved Add Brick VDS command from GlusterBricksResource to GlusterVolumeResource

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/glusterbroker/RemoveBricksFromGlusterVolumeVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/glusterbroker/RemoveBricksFromGlusterVolumeVDSCommand.java
@@ -3,8 +3,9 @@ package org.ovirt.engine.core.vdsbroker.glusterbroker;
 import org.ovirt.engine.core.common.businessentities.GlusterBrickEntity;
 import org.ovirt.engine.core.common.glustercommands.GlusterVolumeBricksVDSParameters;
 
-public class AddBricksToGlusterVolumeVDSCommand extends GlusterBrokerCommand<GlusterVolumeBricksVDSParameters> {
-    public AddBricksToGlusterVolumeVDSCommand(GlusterVolumeBricksVDSParameters parameters) {
+public class RemoveBricksFromGlusterVolumeVDSCommand extends GlusterBrokerCommand<GlusterVolumeBricksVDSParameters> {
+
+    public RemoveBricksFromGlusterVolumeVDSCommand(GlusterVolumeBricksVDSParameters parameters) {
         super(parameters);
     }
 
@@ -16,9 +17,11 @@ public class AddBricksToGlusterVolumeVDSCommand extends GlusterBrokerCommand<Glu
         for (GlusterBrickEntity brick : getParameters().getBricks()) {
             brickList[i++] = brick.getQualifiedName();
         }
-        status = getIrsProxy().glusterVolumeAddBrick(getParameters().getVolumeName(), brickList);
+
+        status = getIrsProxy().glusterVolumeRemoveBrick(getParameters().getVolumeName(), brickList);
 
         // IMPORTANT! This handles errors if any
         ProceedProxyReturnValue();
     }
+
 }
